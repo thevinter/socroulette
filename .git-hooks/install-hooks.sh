d68e2da69c47e8cddd88194ba18d6bae2b4eefba
Valerio Lomanto <valerio.lomanto@sns.it>
2022-01-14 22:22:24 +0100

    begin githook

---
+++ .git-hooks/install-hooks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+this_dir=".git-hooks"
+repo_root="$(git rev-parse --show-toplevel)"
+hooks="pre-commit"
+
+for hook in $hooks; do
+	# the link is relative so it will work even if the repo root is moved
+	ln -s "../../${this_dir}/${hook}" "${repo_root}/.git/hooks/${hook}"
+done
+
