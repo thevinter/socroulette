eadeb759019c926479e5529a70b5a081431421fd
Valerio Lomanto <valerio.lomanto@sns.it>
2022-01-16 15:15:00 +0100

    Add tsconfig and fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,19 @@
-{
-  "extends": "next/core-web-vitals",
-  "rules": {
-	"semi": ["error", "always"]
-  }
-}
-
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['tsconfig.json'],
+  },
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'prefer-const': 'warn',
+  },
+};
