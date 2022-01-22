2292ea32151891b607421795fdea42d51faf9a17
thevinter <theniki98@gmail.com>
2022-01-22 20:23:33 +0100

    fix: refreshing

--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -2,24 +2,18 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import useSWR from 'swr';
 import { useState } from 'react';
-import { Card, CardContent } from '@mui/material';
-import Datalabel from '../../components/datalabel';
 import styles from './user.module.css';
-import Longtext from '../../components/longtext';
-import { SocialIcon } from 'react-social-icons';
 import GeneralInfo from '../../components/info/general_info';
 import InterestsInfo from '../../components/info/interests_info';
 import AppearanceInfo from '../../components/info/appearance_info';
 import Head from 'next/head';
 import MenuItem from '../../components/menuitem';
-import SexualInfo from '../../components/info/interests_info';
 import PrivateInfo from '../../components/info/private_info';
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Success() {
   const { data } = useSWR(`/api/user?id=${useRouter().query.id}`, fetcher);
-  console.log(data);
   const [selected, setSelected] = useState('general');
 
   const selectedSection = (function () {
@@ -47,9 +41,7 @@ export default function Success() {
           rel="stylesheet"
         />
       </Head>
-      <div
-        className="mainWrapper"
-      >
+      <div className="mainWrapper">
         <div style={{ marginBottom: '3px', marginTop: '-30px', fontSize: '3vh' }}>
           <Link href="/">
             <a>homepage</a>
@@ -63,13 +55,8 @@ export default function Success() {
             <a>about</a>
           </Link>
         </div>
-        <div
-          className="mainBox"
-
-        >
-          <div
-            className={styles.content}
-          >
+        <div className="mainBox">
+          <div className={styles.content}>
             <div className={styles.menuWrapper}>
               <div className={styles.menu}>
                 <p
@@ -86,10 +73,10 @@ export default function Success() {
                   style={
                     selected == 'general'
                       ? {
-                        width: '50%',
-                        alignSelf: 'flex-end',
-                        backgroundColor: 'white',
-                      }
+                          width: '50%',
+                          alignSelf: 'flex-end',
+                          backgroundColor: 'white',
+                        }
                       : {}
                   }
                 >
@@ -99,10 +86,10 @@ export default function Success() {
                   style={
                     selected == 'interests'
                       ? {
-                        width: '60%',
-                        alignSelf: 'flex-end',
-                        backgroundColor: 'white',
-                      }
+                          width: '60%',
+                          alignSelf: 'flex-end',
+                          backgroundColor: 'white',
+                        }
                       : {}
                   }
                 >
@@ -112,10 +99,10 @@ export default function Success() {
                   style={
                     selected == 'private'
                       ? {
-                        width: '60%',
-                        alignSelf: 'flex-end',
-                        backgroundColor: 'white',
-                      }
+                          width: '60%',
+                          alignSelf: 'flex-end',
+                          backgroundColor: 'white',
+                        }
                       : {}
                   }
                 >
@@ -125,10 +112,10 @@ export default function Success() {
                   style={
                     selected == 'appearance'
                       ? {
-                        width: '68%',
-                        alignSelf: 'flex-end',
-                        backgroundColor: 'white',
-                      }
+                          width: '68%',
+                          alignSelf: 'flex-end',
+                          backgroundColor: 'white',
+                        }
                       : {}
                   }
                 >
@@ -151,28 +138,54 @@ export default function Success() {
               </div>
             </div>
             <div className={styles.mobileMenu}>
-              <div style={{ display: "flex", justifyContent:"space-evenly", flexDirection: "row" }}>
+              <div
+                style={{ display: 'flex', justifyContent: 'space-evenly', flexDirection: 'row' }}
+              >
                 <p
-                  style={{ textAlign: 'right', fontSize: '1em', color: (selected == 'general' ? "red" : "black"), margin:"7px", borderBottom: '1px solid black' }}
-                  onClick={() => setSelected("general")}
+                  style={{
+                    textAlign: 'right',
+                    fontSize: '1em',
+                    color: selected == 'general' ? 'red' : 'black',
+                    margin: '7px',
+                    borderBottom: '1px solid black',
+                  }}
+                  onClick={() => setSelected('general')}
                 >
                   General
                 </p>
                 <p
-                  style={{ textAlign: 'right', fontSize: '1em', color: (selected == 'interests' ? "red" : "black"), margin:"7px", borderBottom: '1px solid black' }}
-                  onClick={() => setSelected("interests")}
+                  style={{
+                    textAlign: 'right',
+                    fontSize: '1em',
+                    color: selected == 'interests' ? 'red' : 'black',
+                    margin: '7px',
+                    borderBottom: '1px solid black',
+                  }}
+                  onClick={() => setSelected('interests')}
                 >
                   Interests
                 </p>
                 <p
-                  style={{ textAlign: 'right', fontSize: '1em', color: (selected == 'private' ? "red" : "black"), margin:"7px", borderBottom: '1px solid black' }}
-                  onClick={() => setSelected("private")}
+                  style={{
+                    textAlign: 'right',
+                    fontSize: '1em',
+                    color: selected == 'private' ? 'red' : 'black',
+                    margin: '7px',
+                    borderBottom: '1px solid black',
+                  }}
+                  onClick={() => setSelected('private')}
                 >
                   Private
                 </p>
                 <p
-                  style={{ textAlign: 'right', fontSize: '1em', color: (selected == 'appearance' ? "red" : "black"), margin:"7px", borderBottom: '1px solid black' }}
-                  onClick={() => setSelected("appearance")}
+                  style={{
+                    textAlign: 'right',
+                    fontSize: '1em',
+                    color: selected == 'appearance' ? 'red' : 'black',
+                    margin: '7px',
+                    borderBottom: '1px solid black',
+                  }}
+                  onClick={() => setSelected('appearance')}
                 >
                   Appearance
                 </p>
